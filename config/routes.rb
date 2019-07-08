d6aa091a6bd48e184d83ddc1de73a9788abd3870
J Jjhopkins J Hopkins <jenelyn.hopkins@gmail.com>
2019-07-08 10:36:41 -0700

    add comments model, controller and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
- resources :messages
+ devise_for :users
+
+ resources :messages do
+   resources :comments
+ end
 
  root 'messages#index'
 end
